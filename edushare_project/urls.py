4e9ffd46ecb4a50165c76af8729a2a9600eb3288
kali <kali@kali.kali>
2026-01-28 08:28:27 -0500

    Edu share site CEO update

--- edushare_project/urls.py
+++ edushare_project/urls.py
@@ -20,6 +20,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import TemplateView
+from courses.sitemaps import LessonSitemap, CategorySitemap, StaticViewSitemap
+
+sitemaps = {
+    'lessons': LessonSitemap,
+    'categories': CategorySitemap,
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path(os.getenv('ADMIN_URL', 'admin/'), admin.site.urls),
@@ -27,6 +36,8 @@ urlpatterns = [
     path('', include('core.urls')),
     path('courses/', include('courses.urls')),
     path('profile/', include('accounts.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
